507b1301f8a0d971afe7fe0699aa0fa5866f75ec
benhexie <benedictgabriel73@gmail.com>
2023-12-25 22:05:34 +0100

    Update RideCard and DashboardNav CSS

--- src/components/Nav/DashboardNav.jsx
+++ src/components/Nav/DashboardNav.jsx
@@ -1,7 +1,45 @@
+import { NavLink } from "react-router-dom";
 import "./DashboardNav.css";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { useState } from "react";
 
 const DashboardNav = () => {
-  return <div className="dashboard__nav"></div>;
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <>
+      <div
+        className={`dashboard__nav__toggle ${toggle ? "active" : ""}`}
+        onClick={() => setToggle(!toggle)}
+      >
+        <AiOutlineMenu />
+        <AiOutlineClose />
+      </div>
+      <div className={`dashboard__nav ${toggle ? "active" : ""}`}>
+        <div className="dashboard__nav__logo">
+          <NavLink to="" end className="dashboard__nav__logo__container">
+            <img src="https://via.placeholder.com/50" alt="logo" />
+            <span>Pacride</span>
+          </NavLink>
+        </div>
+        <NavLink to="" end className="dashboard__nav__item">
+          Dashboard
+        </NavLink>
+        <NavLink to="my-listings" className="dashboard__nav__item">
+          My Listings
+        </NavLink>
+        <NavLink to="rides" className="dashboard__nav__item">
+          Rides
+        </NavLink>
+        <NavLink to="messages" className="dashboard__nav__item">
+          Messages
+        </NavLink>
+        <NavLink to="settings" className="dashboard__nav__item">
+          Settings
+        </NavLink>
+      </div>
+    </>
+  );
 };
 
 export default DashboardNav;
