1ccea04cc59ddb79ff804c4469d15e152ac8850d
benhexie <benedictgabriel73@gmail.com>
2023-12-29 00:15:45 +0100

    Add loading state and handle contact form submission

--- src/pages/Home/Landing/index.js
+++ src/pages/Home/Landing/index.js
@@ -15,6 +15,7 @@ import peopleSharingRide from "../../../assets/images/people_sharing_ride.jpeg";
 import Input from "../../../components/Input/Input";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import logoImage from "../../../assets/svgs/logo.svg";
+import { toast } from "react-toastify";
 
 const MAPS_API_KEY = process.env.REACT_APP_MAPS_API_KEY;
 
@@ -27,7 +28,9 @@ const Landing = () => {
   const [message, setMessage] = useState("");
   const location = useLocation().pathname;
   const aboutSectionRef = useRef(null);
+  const [loading, setLoading] = useState(false);
   const contactSectionRef = useRef(null);
+  const contactBtnRef = useRef(null);
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: MAPS_API_KEY,
     libraries: ["places"],
@@ -39,11 +42,49 @@ const Landing = () => {
     else if (/\/contact/.test(location))
       contactSectionRef.current?.scrollIntoView({ behavior: "smooth" });
     else window.scrollTo(0, 0);
-  }, [location])
+  }, [location]);
+
+  useEffect(() => {
+    if (loading) {
+      document.body.style.cursor = "wait";
+      contactBtnRef.current.disabled = true;
+      return;
+    }
+    document.body.style.cursor = "default";
+    contactBtnRef.current.disabled = false;
+  }, [loading])
 
   const handleSubmit = (e) => {
     e.preventDefault();
     navigate(`/dashboard/listings?from=${from}&to=${to}`);
+  };
+
+  const handleContactSubmit = async (e) => {
+    e.preventDefault();
+    if (!email || !message) return;
+
+    try {
+      setLoading(true);
+      const response = await fetch(`${process.env.REACT_APP_SERVER}/contact`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, subject, message }),
+      });
+      const data = await response.json();
+      setLoading(false);
+      if (data.error) {
+        return toast.error(data.message);
+      }
+      toast.success(data.message);
+      setEmail("");
+      setSubject("");
+      setMessage("");
+    } catch (error) {
+      setLoading(false);
+      toast.error("Something went wrong");
+    }
   };
 
   return (
@@ -121,7 +162,10 @@ const Landing = () => {
       <section className="landing__contact__section" ref={contactSectionRef}>
         <div className="landing__contact__container">
           <h2 className="landing__contact__title">Contact us</h2>
-          <form className="landing__contact__form">
+          <form
+            className="landing__contact__form"
+            onSubmit={handleContactSubmit}
+          >
             <Input
               type="email"
               placeholder="Email"
@@ -145,7 +189,7 @@ const Landing = () => {
               onChange={(e) => setMessage(e.target.value)}
               required
             />
-            <Button className="landing__contact__button">Send</Button>
+            <Button ref={contactBtnRef} className="landing__contact__button">Send</Button>
           </form>
         </div>
       </section>
